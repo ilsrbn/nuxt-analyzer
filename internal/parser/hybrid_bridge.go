551a50ef5efe9725519217d991fa3d08055713ef
ilsrbn <serbini271@gmail.com>
2026-05-08 12:59:39 +0300

    fix: hybrid_bridge handle .tsx/.jsx extensions

--- internal/parser/hybrid_bridge.go
+++ internal/parser/hybrid_bridge.go
@@ -49,7 +49,7 @@ func (h *HybridBridge) Parse(files []string, autoImportNames []string) ([]Parsed
 		switch strings.ToLower(filepath.Ext(file)) {
 		case ".vue":
 			vueFiles = append(vueFiles, file)
-		case ".ts":
+		case ".ts", ".tsx", ".jsx":
 			tsFiles = append(tsFiles, file)
 		}
 	}
